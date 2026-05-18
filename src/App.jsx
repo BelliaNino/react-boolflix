671cdcc5923872a39f6604928349c7589aa1f79f
BelliaNino <bellianino1996@gmail.com>
2026-05-18 13:08:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer"
+import Body from "./components/Body"
+import Header from "./components/Header"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>  
+    </>
+  )
+}
+
+export default App
